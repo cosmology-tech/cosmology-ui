29a0bbf553262dcbe9d1de15034f48e4b8dcc6d7
yujulchen <74940804+yujulchen@users.noreply.github.com>
2023-02-20 10:55:25 +0800

    add type

--- packages/utils/types/cosmos/modals/SimpleConnectModal.d.ts
+++ packages/utils/types/cosmos/modals/SimpleConnectModal.d.ts
@@ -1,14 +1,16 @@
 /// <reference types="react" />
 import { SimpleConnectModalType } from '../../index';
-export declare const SimpleConnectModalBaseStyle: () => {
+export declare const SimpleConnectModalBaseStyle: (theme: string) => {
     position: string;
     alignSelf: string;
     borderRadius: string;
+    overflow: string;
     w: string;
     mx: number;
+    bg: string;
+    color: string;
     _focus: {
         outline: string;
     };
-    overflow: string;
 };
 export declare const SimpleConnectModal: ({ initialRef, modalView, className, styleProps, modalOpen, modalOnClose }: SimpleConnectModalType) => JSX.Element;
